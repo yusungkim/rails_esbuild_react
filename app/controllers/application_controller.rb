c42871ab54e19f2a8e1994bd5238c2422730a92a
Yuson.Kim <yuson.kim@glv.co.jp>
2025-03-29 14:50:23 +0900

    page not found for template missing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  rescue_from ActionView::MissingTemplate do |exception|
+    render status: :not_found, plain: "Page Not Found"
+  end
 end
